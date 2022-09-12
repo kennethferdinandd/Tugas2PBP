694327a3bd785b8a15d9784f5c1e1a348abe3f29
kennethferdinandd <kennethferdinand03@gmail.com>
2022-09-12 20:32:05 +0700

    Menambahkan tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+
+def show_katalog(request):
+    return render(request, "katalog.html", context)
+
+
+data_katalog = CatalogItem.objects.all()
+context = {
+    'list_katalog': data_katalog,
+    'nama': 'Kenneth',
+    'id': '2106750282',
+}
