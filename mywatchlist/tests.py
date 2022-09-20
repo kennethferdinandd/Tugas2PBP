6a2374ec0c00156deb55639fa71cf1f1558c4186
kennethferdinandd <kennethferdinand03@gmail.com>
2022-09-20 21:50:23 +0700

    Fixing files for Assignment 3

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.urls import reverse
 # Create your tests here.
+
+class MyWatchListTest(TestCase):
+
+        def test_url_html(self):
+            self.client = Client()
+            response = self.client.get(reverse('mywatchlist:show_mywatchlist'))
+            self.assertEqual(response.status_code, 200)
+
+        def test_url_xml(self):
+            self.client = Client()
+            response = self.client.get(reverse('mywatchlist:show_xml'))
+            self.assertEqual(response.status_code, 200)
+
+        def test_url_json(self):
+            self.client = Client()
+            response = self.client.get(reverse('mywatchlist:show_json'))
+            self.assertEqual(response.status_code, 200)
